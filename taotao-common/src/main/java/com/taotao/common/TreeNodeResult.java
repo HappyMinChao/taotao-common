55f13d9d67a8b8a624e6b580f8955508443197a8
HappyMinChao <du_minchao@163.com>
2016-08-10 18:59:23 +0800

    开发完ftpUtil未实现图片的上传

---
+++ taotao-common/src/main/java/com/taotao/common/TreeNodeResult.java
@@ -0,0 +1,50 @@
+package com.taotao.common;
+
+public class TreeNodeResult {
+	private long id;
+	private String text;
+	private String state;
+
+	public TreeNodeResult() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public TreeNodeResult(long id, String text, String state) {
+		super();
+		this.id = id;
+		this.text = text;
+		this.state = state;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	@Override
+	public String toString() {
+		return "TreeNodeResult [id=" + id + ", text=" + text + ", state="
+				+ state + "]";
+	}
+
+}
